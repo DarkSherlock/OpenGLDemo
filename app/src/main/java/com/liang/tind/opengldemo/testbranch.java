7a71f9b2398e45b447e422d14d35883d1bc597cf
Sherlock <tind.liang@foxmail.com>
2018-04-01 20:50:13 +0800

    test branch

--- app/src/main/java/com/liang/tind/opengldemo/testbranch.java
+++ app/src/main/java/com/liang/tind/opengldemo/testbranch.java
@@ -2,12 +2,7 @@ package com.liang.tind.opengldemo;
 
 /**
  * Created by Sherlock on 2018/4/1.
- *
- *
  */
 
-public class TestRevert {
-
-    static final String STRING = "";
-
+public class testbranch {
 }
